d494cc7fd21c7c9a56fd2fc949db7eb9bda988a8
Kohe Tokoi <tokoik@gmail.com>
2012-09-03 17:43:39 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,14 @@ static void display(void)
   // ‰æ–ÊƒNƒŠƒA
   glClear(GL_COLOR_BUFFER_BIT);
   
+  glColor3d(1.0, 0.0, 0.0);
+  glBegin(GL_LINE_LOOP);
+  glVertex2d(-0.9, -0.9);
+  glVertex2d( 0.9, -0.9);
+  glVertex2d( 0.9,  0.9);
+  glVertex2d(-0.9,  0.9);
+  glEnd();
+  
   std::cerr << "display:" << ++frame << std::endl;
 }
 
